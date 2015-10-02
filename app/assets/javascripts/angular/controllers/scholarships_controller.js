59f58847635f84843d6baba7e6a44b6967da96cd
newOnahtaN <ncowen@email.wm.edu>
2015-10-02 00:02:34 -0400

    added loading bar and got filters to work fully

--- app/assets/javascripts/angular/controllers/scholarships_controller.js
+++ app/assets/javascripts/angular/controllers/scholarships_controller.js
@@ -3,11 +3,11 @@ angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsCo
   $scope.searchType = "Advanced"
   $scope.filters = {studentTypes: [], areasOfStudy: [], purposes: [], regions: [], seasons: [], genders: [], citizenships: [], financialNeeds: []};
   $scope.studentTypes = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Grad Student', 'Alumni'];
-  $scope.areasOfStudy = ['Sciences', 'Social Sciences', 'Humanities', 'Education', 'Math and Technology', 'Health Disciplines', 'Law', 'Fine and Performing Arts'];
-  $scope.purposes = ['Graduate/PostGrad Study', 'International Study, Research, or Internships', 'Public Service', 'Undergraduate Tuition', 'Job Placement/Funding', 'Language'];
+  $scope.areasOfStudy = ['Natural Science', 'Social Science', 'Humanities', 'Education', 'Math and Technology', 'Health Disciplines', 'Law', 'Fine and Performing Arts'];
+  $scope.purposes = ['Graduate/Postgrad Study', 'International Study, Research, or Internships', 'Public Service', 'Undergraduate Tuition', 'Job Placement/Funding', 'Language'];
   $scope.regions = ['Europe/Australia', 'United States', 'Americas (not U.S.)', 'Africa', 'Asia'];
   $scope.seasons = ['Spring', 'Summer', 'Fall'];
-  $scope.genders = ['Male', 'Female'];
+  $scope.genders = ['Open to Men', 'Open to Women'];
   $scope.citizenships = ["Citizenship required", "Citizenship not required"];
   $scope.financialNeeds = ["Financial need required", "Financial need not required"]
 
@@ -15,6 +15,7 @@ angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsCo
   $http.get('/MasterList').success(function(response) {
     master_list = refineMasterList(response)
     $scope.originalList = master_list;
+    console.log(master_list);
     $scope.scholarships = angular.copy(master_list);
   });
 
@@ -22,6 +23,17 @@ angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsCo
     for(i = 0; i < list.length; i++){
         list[i].title = list[i]["Scholarship/Fellowship Title"];
         list[i].link = list[i]["External Website Link"];
+        list[i]["Freshman"] = list[i]["Applying as a: Freshman"];
+        list[i]["Natural Science"] = list[i]["Area of Study Science/Enviro"];
+        list[i]["United States"] = list[i]["U.S."];
+        list[i]["Europe/Australia"] = list[i]["World Region Europe/Australia"];
+        list[i]["Spring"] = list[i]["Semester of Scholarship Duration Spring"];
+        list[i]["Open to Men"] = list[i]["Gender Specific Open to Men"];
+        list[i]["Citizenship required"] = list[i]["U.S. Citizen Req."];
+        list[i]["Citizenship not required"] = !list[i]["U.S. Citizen Req."];
+        list[i]["Financial need required"] = list[i]["Financial Need Required?"];
+        list[i]["Financial need not required"] = !list[i]["Financial Need Required?"];
+        list[i]["Graduate/Postgrad Study"] = list[i]["Purpose Graduate/Postgrad Study"];
         delete list[i]["Scholarship/Fellowship Title"];
         delete list[i]["External Website Link"];
     }
@@ -31,8 +43,10 @@ angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsCo
   $scope.toggleSearchType = function () {
     if ($scope.searchType === "Advanced"){
       $scope.searchType = "Basic"
+      restoreFilters();
     } else {
       $scope.searchType = "Advanced"
+      clearFilters();
     }
   }
 
@@ -47,6 +61,7 @@ angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsCo
           filter = filters[j];
           if (!scholarship[filter]) {
             scholarships.splice(i,1);
+            break;
           }
         }
       }
@@ -62,6 +77,19 @@ angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsCo
       }
     }
     return temp;
+  }
+
+  clearFilters = function () {
+    $scope.savedFilters = angular.copy($scope.filters);
+    for (var filterType in $scope.filters) {
+      if ($scope.filters.hasOwnProperty(filterType)) {
+        $scope.filters[filterType] = [];
+      }
+    }
+  }
+
+  restoreFilters = function () {
+    $scope.filters = $scope.savedFilters;
   }
 
 }]);
