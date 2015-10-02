4d246e8b681b80dc7f88fa1323b3543c9c11eec6
newOnahtaN <ncowen@email.wm.edu>
2015-10-02 12:51:04 -0400

    got rid of bindonce dependency, uses builtins only now

--- app/assets/javascripts/angular/controllers/scholarships_controller.js
+++ app/assets/javascripts/angular/controllers/scholarships_controller.js
@@ -1,4 +1,4 @@
-angular.module("PSAScholarships", ['angular.filter','pasvaz.bindonce']).controller('ScholarshipsController', ['$scope', '$http', function($scope, $http){
+angular.module("PSAScholarships", ['angular.filter']).controller('ScholarshipsController', ['$scope', '$http', function($scope, $http){
 
   $scope.searchType = "Advanced"
   $scope.filters = {studentTypes: [], areasOfStudy: [], purposes: [], regions: [], seasons: [], genders: [], citizenships: [], financialNeeds: []};
